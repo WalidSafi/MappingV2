ed3a4b25e9a68edb628fb9a9d5bd37b85ebe676f
WalidSafi <32139490+WalidSafi@users.noreply.github.com>
2020-12-07 21:05:17 -0500

    Add files via upload

--- Server.java
+++ Server.java
@@ -4,23 +4,36 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+import java.lang.Object;
+import java.lang.Runtime;
+import java.util.concurrent.Semaphore;
+import java.util.Date;
+import java.time.*;
 
 public class Server {
 
     public static ArrayList<String> clientdata = new ArrayList<String>();
     public static ArrayList<String> locations = new ArrayList<String>();
+    public static int counter = 0;
 
     public static void main(String[] args) {
 
-        try { ServerSocket s = new ServerSocket(5556);
+        Semaphore sem = new Semaphore(1);
+
+        try {
+
+            ServerSocket s = new ServerSocket(5556);
+
             while (true) {
                 Socket socket = s.accept();
                 Handler sc = new Handler(socket);
                 new Thread(sc).start();
+                // Thread(sc).join();
             }
         }
         catch (IOException e) {
-            e.printStackTrace();
+
         }
     }
 
@@ -35,6 +48,8 @@ public class Server {
         @Override
         public void run() {
 
+            Semaphore sem = new Semaphore(1);
+
             try {
 
                 // Creating output and input data streams
@@ -46,8 +61,12 @@ public class Server {
                 // Information from taken from Client is saved in testData arraylist
                 clientdata = (ArrayList<String>) list;
 
+                sem.acquire();
+
                 // adds location data to the main list (Doesnt delete if the client disconnects)
                 addClientData(clientdata);
+
+                sem.release();
 
                 //Sending back an array list back to the client
                 oos.writeObject(locations);
@@ -56,6 +75,8 @@ public class Server {
 
             } catch (ClassNotFoundException e){
 
+            } catch(InterruptedException ie){
+                ie.printStackTrace();
             }
 
         }
@@ -67,6 +88,11 @@ public class Server {
         String name = null;
         name = clientdata.get(2);
 
+        if(name.equals("")){
+            System.out.println("No name Detected");
+            return;
+        }
+
         if (locations.contains(name)){
             System.out.println(name + " already exists in the list");
 
@@ -75,17 +101,43 @@ public class Server {
             System.out.println("Old Co-ordinates " + locations);
             locations.set((index - 2), client.get(0));
             locations.set((index - 1), client.get(1));
-
             System.out.println("Near Co-ordinates " + locations + "\n");
+
         } else {
             locations.add(client.get(0));
             locations.add(client.get(1));
             locations.add(client.get(2));
+            serverLog(name);
             System.out.println("Added New Location for " + name + " Co-ordinates = " + locations + "\n");
         }
     }
 
-    public static void serverLog(){
+    class clientdatathread extends Thread{
+        Semaphore sem;
+        String clientthread;
+        public clientdatathread(Semaphore sem, String clientthread){
+            super(clientthread);
+            this.sem = sem;
+            this.clientthread = clientthread;
+        }
+    }
+
+    public static void serverLog(String name){
+
+        try{
+            ZonedDateTime s = ZonedDateTime.now();
+
+            File logfile = new File("log.txt");
+            logfile.createNewFile();
+
+            FileWriter logwrite = new FileWriter("log.txt", true); // Allowing to append to file
+
+            logwrite.write(name + " Has Been Added to the Server Data at Date Time: " + s + "\n");
+
+            logwrite.close();
+        } catch (IOException e){
+
+        }
 
     }
 }
